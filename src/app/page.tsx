7366341e1efa01527e29973a8d28fc2f707c8c31
vmwork <v.marwork@gmail.com>
2025-09-08 12:28:03 +0300

    fastapi + Next

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function MyNextFastAPIApp() {
+  const getRole = async () => {
+    let baseUrl = 'https://my-next-fastapi-app.vercel.app';
+    if (process.env.NODE_ENV === 'development') {
+      baseUrl = 'http://localhost:3000';
+    }
+    const title = 'Frontend Developer';
+    try {
+      const response = await fetch(
+        `${baseUrl}/api/py/engineer-roles?title=${title}`
+      );
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const someRole = await response.json();
+      setRole(someRole);
+    } catch (error) {
+      console.error('Error fetching engineer role:', error);
+      return null;
+    }
+  };
+  const [role, setRole] = useState({});
+  return (
+    <>
+      <button
+        className="border rounded-sm p-4 cursor-pointer bg-amber-200 "
+        onClick={getRole}
+      >
+        Get role
+      </button>
+      <div>{`The main skill of a ${role?.title} is ${role?.mainskill}.`}</div>
+    </>
+  );
+}
