2f1eca81b65b53598456d8e47b48d7c566b87b16
vmwork <v.marwork@gmail.com>
2025-09-08 11:14:47 +0300

    add

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import  FastAPI
+from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from dataBase import create_tables, delete_tables
 from router import router as tasks_router
