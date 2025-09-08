80bf6b091fda1c851346a01bf45adbc84eab245e
vmwork <v.marwork@gmail.com>
2025-09-08 10:14:59 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import  FastAPI
+from contextlib import asynccontextmanager
+from dataBase import create_tables, delete_tables
+from router import router as tasks_router
+
+asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    await create_tables()
+    yield
+    print("Shutting down...")
+
+
+app = FastAPI(lifespan=lifespan) 
+app.include_router(tasks_router)
+
